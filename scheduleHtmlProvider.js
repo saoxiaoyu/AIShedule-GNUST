f3b04dd2d4aba27e6cbbe015474faa81ef1c6f96
saoxiaoyu <wangyu@xiaoyu.ac.cn>
2024-08-21 20:02:28 +0800

    update

--- scheduleHtmlProvider.js
+++ scheduleHtmlProvider.js
@@ -1,17 +1,6 @@
 async function scheduleHtmlProvider() {
     await loadTool('AIScheduleTools');
-
-    try {
-        let frmDesk = document.getElementById('frmDesk').contentWindow.document;
-        let frame_1 = frmDesk.getElementById("frame_1").contentWindow.document;
-        let frmReport = frame_1.getElementById("frmReport").contentWindow.document;
-        let tableElement = frmReport.getElementById("mytable");
-
-        return tableElement.innerHTML;
-    }
-    catch(e) {
-        await AIScheduleAlert("请点击主控-教学安排，然后点击导入课表");
-        return 'do not continue';
-    }
-
+    await AIScheduleAlert("请点击最近使用-教学安排，然后点击导入课表");
+    return 'do not continue';
+    
 }
--- scheduleTimer.js
+++ scheduleTimer.js
@@ -3,57 +3,53 @@ async function scheduleTimer({
   parserRes
 } = {}) {
   return {
-    totalWeek: 20, // 总周数：[1, 30]之间的整数
-    startSemester: '', // 开学时间：时间戳，13位长度字符串，推荐用代码生成
+    totalWeek: 18, // 总周数：[1, 30]之间的整数
+    startSemester: '1725206400000', // 开学时间：时间戳，13位长度字符串，推荐用代码生成
     startWithSunday: false, // 是否是周日为起始日，该选项为true时，会开启显示周末选项
     showWeekend: true, // 是否显示周末
     forenoon: 4, // 上午课程节数：[1, 10]之间的整数
     afternoon: 4, // 下午课程节数：[0, 10]之间的整数
-    night: 3, // 晚间课程节数：[0, 10]之间的整数
+    night: 2, // 晚间课程节数：[0, 10]之间的整数
     sections: [{
       section: 1,
-      startTime: '08:10',
-      endTime: '08:55',
+      startTime: '08:30',
+      endTime: '09:15',
     },{
       section: 2,
-      startTime: '09:05',
-      endTime: '09:50',
+      startTime: '09:20',
+      endTime: '10:05',
     },{
       section: 3,
-      startTime: '10:20',
-      endTime: '11:05',
+      startTime: '10:25',
+      endTime: '11:10',
     },{
       section: 4,
       startTime: '11:15',
       endTime: '12:00',
     },{
       section: 5,
-      startTime: '14:30',
-      endTime: '15:15',
+      startTime: '14:00',
+      endTime: '14:45',
     },{
       section: 6,
-      startTime: '15:25',
-      endTime: '16:10',
+      startTime: '14:50',
+      endTime: '15:35',
     },{
       section: 7,
-      startTime: '16:20',
-      endTime: '17:05',
+      startTime: '15:55',
+      endTime: '16:40',
     },{
       section: 8,
-      startTime: '17:15',
-      endTime: '18:00',
+      startTime: '16:45',
+      endTime: '17:30',
     },{
       section: 9,
-      startTime: '19:20',
-      endTime: '20:05',
+      startTime: '19:00',
+      endTime: '19:45',
     },{
       section: 10,
-      startTime: '20:15',
-      endTime: '21:00',
-    },{
-      section: 11,
-      startTime: '21:10',
-      endTime: '21:55',
+      startTime: '19:50',
+      endTime: '20:35',
     }],
   }
 }
